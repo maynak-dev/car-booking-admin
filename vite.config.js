b2a8f4faa430d3470ae2b76ea409e976ce01c008
maynak-dev <work.maynak@gmail.com>
2026-03-13 18:53:20 +0530

    vite config changed2

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
-export default defineConfig({
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+
+module.exports = defineConfig({
   plugins: [react()],
   base: '/admin/',
+  server: { port: 5174 },
 });
